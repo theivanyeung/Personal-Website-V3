69868d4bab25c329d942e6ee3a8b7205b6f06e7b
Ivan Yeung <yeungivan09@gmail.com>
2023-05-29 21:34:56 -0700

    updated hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -9,32 +9,26 @@ import customTheme from "../styles/customTheme";
 import OpticalCircle from "../TARS/OpticalCircle";
 import VideoBackground from "../components/VideoBackground";
 
-const HotJar = () => {
-  return (
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-        (function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:3512480,hjsv:6};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        `,
-      }}
-    />
-  );
-};
-
 const GlobalHead = () => {
   return (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <HotJar />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3512480,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+        }}
+      />
     </Head>
   );
 };
