599705d0045699bb4e21a6003fa0eb5d5c5913c2
Ivan Yeung <yeungivan09@gmail.com>
2023-05-29 21:26:35 -0700

    updated hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -9,26 +9,32 @@ import customTheme from "../styles/customTheme";
 import OpticalCircle from "../TARS/OpticalCircle";
 import VideoBackground from "../components/VideoBackground";
 
+const HotJar = () => {
+  return (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+        (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:3512480,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `,
+      }}
+    />
+  );
+};
+
 const GlobalHead = () => {
   return (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:3362166,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `,
-        }}
-      />
+      <HotJar />
     </Head>
   );
 };
@@ -36,25 +42,7 @@ const GlobalHead = () => {
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={customTheme}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:3362166,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `,
-          }}
-        />
-      </Head>
+      <GlobalHead />
       <VideoBackground
         videoSrc={
           "https://storage.googleapis.com/ivan-yeung-personal/snn-background.mp4"
