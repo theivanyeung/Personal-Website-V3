8b0bdd03b92d6fb3223ab71426cbd4b43b01a3dc
Ivan Yeung <yeungivan09@gmail.com>
2023-05-29 20:53:41 -0700

    added hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,25 @@ import customTheme from "../styles/customTheme";
 import OpticalCircle from "../TARS/OpticalCircle";
 import VideoBackground from "../components/VideoBackground";
 
+const HotJar = () => {
+  return (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+        (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:3512480,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `,
+      }}
+    />
+  );
+};
+
 const GlobalHead = () => {
   return (
     <Head>
